2b149202f30dab9349869a257a194d18fb175bd0
neji <gmail>
2022-05-09 16:43:38 +0700

    change v1.0.3

--- LibTest1/src/main/java/com/example1/libtest1/Sumga.kt
+++ LibTest1/src/main/java/com/example1/libtest1/Sumga.kt
@@ -3,7 +3,7 @@ package com.example1.libtest1
 class Sumga {
     companion object {
         fun show(): String {
-            return "Sumga chao moi nguoi"
+            return "Sumga chao moi nguoi. hihi"
         }
     }
 }
